a03614614d90bbed33029ce9052c2990f9aeae42
ColtonJE <33628143+ColtonJE@users.noreply.github.com>
2021-11-28 20:54:14 -0500

    fixed the one bug

--- flaskProject/modeltest.py
+++ flaskProject/modeltest.py
@@ -131,23 +131,23 @@ t = time.time()
 # X_test = vectoriser.transform(X_test)
 
 
-def model_evaluate(model):
-    y_pred = model.predict(X_test)
-    print(classification_report(y_test, y_pred))
-    cm = confusion_matrix(y_test, y_pred)
-
-    categories = ['Negative', 'Positive']
-    group_names = ['True Negative', 'False Positive', 'False Negative', 'True Positive']
-    group_percentages = ['{0:.2%}'.format(value) for value in cm.flatten() / np.sum(cm)]
-    labels = [f'{v1}\n{v2}' for v1, v2 in zip(group_names, group_percentages)]
-    labels = np.asarray(labels).reshape(2, 2)
-
-    sns.heatmap(cm, annot=labels, cmap='Blues', fmt='',
-                xticklabels=categories, yticklabels=categories)
-
-    plt.xlabel("Predicted values", fontdict={'size': 14}, labelpad=10)
-    plt.ylabel("Actual values", fontdict={'size': 14}, labelpad=10)
-    plt.title("Confusion Matrix", fontdict={'size': 18}, pad=20)
+# def model_evaluate(model):
+#     y_pred = model.predict(X_test)
+#     print(classification_report(y_test, y_pred))
+#     cm = confusion_matrix(y_test, y_pred)
+#
+#     categories = ['Negative', 'Positive']
+#     group_names = ['True Negative', 'False Positive', 'False Negative', 'True Positive']
+#     group_percentages = ['{0:.2%}'.format(value) for value in cm.flatten() / np.sum(cm)]
+#     labels = [f'{v1}\n{v2}' for v1, v2 in zip(group_names, group_percentages)]
+#     labels = np.asarray(labels).reshape(2, 2)
+#
+#     sns.heatmap(cm, annot=labels, cmap='Blues', fmt='',
+#                 xticklabels=categories, yticklabels=categories)
+#
+#     plt.xlabel("Predicted values", fontdict={'size': 14}, labelpad=10)
+#     plt.ylabel("Actual values", fontdict={'size': 14}, labelpad=10)
+#     plt.title("Confusion Matrix", fontdict={'size': 18}, pad=20)
 
 
 # t = time.time()
